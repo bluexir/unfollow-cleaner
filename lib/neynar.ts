89629984123d5f1ec66a443eb5212ec93edab9a4
bluexir <127672819+bluexir@users.noreply.github.com>
2025-12-23 18:06:35 +0300

    Update error message and fix import quotes gemini 1.2

--- lib/neynar.ts
+++ lib/neynar.ts
@@ -1,11 +1,10 @@
-import { NeynarAPIClient } from '@neynar/nodejs-sdk';
+import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 
 if (!process.env.NEYNAR_API_KEY) {
-  throw new Error('NEYNAR_API_KEY is not set');
+  throw new Error("NEYNAR_API_KEY ortam değişkeni eksik!");
 }
 
 export const neynarClient = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
 
-// Constants
-export const REQUIRED_FOLLOW_FID = 429973; // @bluexir FID
+export const REQUIRED_FOLLOW_FID = 429973;
 export const REQUIRED_FOLLOW_USERNAME = 'bluexir';
