1b52dafa2fbcb06af47132dc23266525e38bb1eb
bluexir <127672819+bluexir@users.noreply.github.com>
2025-12-29 21:23:49 +0300

    Update neynar.ts

--- lib/neynar.ts
+++ lib/neynar.ts
@@ -4,6 +4,4 @@ if (!process.env.NEYNAR_API_KEY) {
   throw new Error("NEYNAR_API_KEY ortam değişkeni eksik!");
 }
 
-export const neynarClient = new NeynarAPIClient({
-  apiKey: process.env.NEYNAR_API_KEY,
-});
+export const neynarClient = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
