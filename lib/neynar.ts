15596a6ec01bcd39e1a8f5ba8093f08d8263afc1
bluexir <127672819+bluexir@users.noreply.github.com>
2026-01-21 07:37:22 +0300

    Update neynar.ts

--- lib/neynar.ts
+++ lib/neynar.ts
@@ -4,19 +4,19 @@ if (!process.env.NEYNAR_API_KEY) {
   throw new Error("NEYNAR_API_KEY ortam değişkeni eksik!");
 }
 
-const rawConfig = new Configuration({
-  apiKey: process.env.NEYNAR_API_KEY,
-});
+export const neynarClientRaw = new NeynarAPIClient(
+  new Configuration({
+    apiKey: process.env.NEYNAR_API_KEY,
+  })
+);
 
-const visibleConfig = new Configuration({
-  apiKey: process.env.NEYNAR_API_KEY,
-  baseOptions: {
-    headers: {
-      "x-neynar-experimental": "true",
+export const neynarClientVisible = new NeynarAPIClient(
+  new Configuration({
+    apiKey: process.env.NEYNAR_API_KEY,
+    baseOptions: {
+      headers: {
+        "x-neynar-experimental": "true",
+      },
     },
-  },
-});
-
-export const neynarClientRaw = new NeynarAPIClient(rawConfig);
-export const neynarClientVisible = new NeynarAPIClient(visibleConfig);
-export const neynarClient = neynarClientVisible;
+  })
+);
