0fa6a83ee957c0fa06e79d740dd362581493dd79
bluexir <127672819+bluexir@users.noreply.github.com>
2026-01-23 20:47:41 +0300

    Update route.ts

--- app/api/create-signer/route.ts
+++ app/api/create-signer/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from 'next/server';
 import * as ed from '@noble/ed25519';
 import { mnemonicToAccount } from 'viem/accounts';
